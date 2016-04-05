54cdabda7ff6ccfe5fc8e43801041b6bf07b3760
Joey Lee <s3589@hotmail.com>
2016-04-05 13:01:58 -0700

    added game
    added game from CPSC314

---
+++ PlayerMesh.js
@@ -0,0 +1,144 @@
+THREE.PlayerMesh = function (geometry, material) {
+
+	THREE.Mesh.call( this, geometry, material );
+
+	this.type = 'PlayerMesh';
+
+	this.geometry = geometry !== undefined ? geometry : new THREE.Geometry();
+	this.material = material !== undefined ? material : new THREE.MeshNormalMaterial();
+
+	this.size = geometry.boundingSphere.radius;
+
+	this.clock = new THREE.Clock(true);
+
+	this.velocity = new THREE.Vector3(0,0,0);
+
+	this.maxJumps = 2;		// number of jumps allowed
+	this.jumpCounter = 0;	// current number of jumps
+	this.gravity = -10;
+	this.maxV = 300		// max velocity in any direction
+
+	this.floorHeight = this.size*2-1.1;
+	this.platformHeight = this.floorHeight;
+
+	// flags for collision in each direction. 0 = no collision, -1 and 1 = collision on one of the sides
+	// ie: (0,-1,0) means collision with floor
+	this.collisions = new THREE.Vector3(0,0,0);
+
+	// Set the rays : one vector for every potential direction
+    this.rays = [
+    //rays for checking collision in Y (up/down) 
+    new THREE.Vector3(0, -1, 0),
+    new THREE.Vector3(0, 1, 0),
+
+    //rays for checking collision in X
+    new THREE.Vector3(-1, 0, 0),
+    new THREE.Vector3(1, 0, 0),
+
+    //rays for checking collision in Z
+    new THREE.Vector3(0, 0, -1),
+    new THREE.Vector3(0, 0, 1), 
+
+    // diagonal directions
+    new THREE.Vector3(1, 0, 1),
+    new THREE.Vector3(1, 0, -1),
+    new THREE.Vector3(-1, 0, 1),
+    new THREE.Vector3(-1, 0, -1),
+
+    new THREE.Vector3(0, 1, 1),
+    new THREE.Vector3(0, 1, -1),
+    new THREE.Vector3(0, -1, 1),
+    new THREE.Vector3(0, -1, -1),
+
+    new THREE.Vector3(1, 1, 0),
+    new THREE.Vector3(1, -1, 0),
+    new THREE.Vector3(-1, 1, 0),
+    new THREE.Vector3(-1, -1, 0)
+	];
+
+	// And the "RayCaster", able to test for intersections
+    this.caster = new THREE.Raycaster();
+
+	this.drawMode = THREE.TrianglesDrawMode;
+	this.updateMorphTargets();
+};
+
+THREE.PlayerMesh.prototype = Object.create( THREE.Mesh.prototype);
+THREE.PlayerMesh.prototype.constructor = THREE.PlayerMesh;
+
+THREE.PlayerMesh.prototype.jump = function () {
+	var Vy = this.velocity.y
+	if (this.jumpCounter < this.maxJumps) {
+		this.velocity.setY(300);
+		this.jumpCounter += 1;
+	}	
+}
+
+THREE.PlayerMesh.prototype.updateVelocity = function () {
+	var Vy = Math.min(this.velocity.y + this.gravity, this.maxV);
+	var Cy = this.collisions.y;
+
+	// change velocities to 0 when object hits obstacle
+	if (Cy == -1 && Vy <= 0) {			// collision with floor
+		this.velocity.setY(0);
+		this.jumpCounter = 0;
+	} else if (Cy == 1 && Vy >= 0) {	// collision with ceiling
+		this.velocity.setY(0);
+	} else {							// no collision
+		this.velocity.setY(Vy);
+	}
+}
+
+THREE.PlayerMesh.prototype.updatePosition = function () {
+	this.CollisionCheck();
+	this.updateVelocity();
+
+	var dT = this.clock.getDelta();
+	var Py = this.position.y + this.velocity.y*dT;
+
+	if (Py < this.platformHeight) {
+		this.position.setY(this.platformHeight);
+	} else {
+		this.position.setY(Py);
+	}	
+}
+
+THREE.PlayerMesh.prototype.CollisionCheck = function () {
+	this.collisions.set(0,0,0);
+
+	for (i = 0; i < this.rays.length; i += 1) {
+	  // We reset the raycaster to this direction
+	  this.caster.set(this.position, this.rays[i]);
+	  // Test if we intersect with any obstacle mesh
+	  collisions = this.caster.intersectObjects(allObstacles);
+	  // And flag for collision if we do
+	  if (collisions.length > 0 && collisions[0].distance <= this.size) {
+	    if ([0,10,11,13,15].indexOf(i) != -1) {
+	    	this.collisions.setY(-1);
+	    	this.platformHeight = collisions[0].point.y; //set height of current platform
+	    	this.translateY(this.size-collisions[0].distance);		//shift player so it's just touching the edge 
+	    } else if ([1,8,9,12,14].indexOf(i) != -1) {
+	    	this.collisions.setY(1);
+	    	this.translateY(-(this.size-collisions[0].distance));	//shift player so it's just touching the edge 
+	    }
+
+	    if ([2].indexOf(i) != -1) {	    	
+	    	this.collisions.setX(-1);
+	    	this.translateX(this.size-collisions[0].distance);		//shift player so it's just touching the edge 
+	    } else if ([3].indexOf(i) != -1) {
+	    	this.collisions.setX(1);
+	    	this.translateX(-(this.size-collisions[0].distance));	//shift player so it's just touching the edge 
+	    }
+
+	    if ([4].indexOf(i) != -1) {
+	    	this.collisions.setZ(-1);
+	    	this.translateZ(this.size-collisions[0].distance);		//shift player so it's just touching the edge 
+	    } else if ([5].indexOf(i) != -1) {
+	    	this.collisions.setZ(1);
+	    	this.translateZ(-(this.size-collisions[0].distance));	//shift player so it's just touching the edge 
+	    }
+	  } else { // no collisions so the ball is allowed to fall
+	  	this.platformHeight = this.floorHeight;
+	  }
+	}
+}
---
+++ p4.js
@@ -0,0 +1,179 @@
+// CPSC 314 Final project: 
+// Game name: 
+
+var scene = new THREE.Scene();
+
+// setting up renderer
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setClearColor(0xBBBBBBB);
+document.body.appendChild(renderer.domElement);
+
+
+  // setting up window resize and adaptation
+function resize() {
+	windowWidth = window.innerWidth;
+	windowHeight = window.innerHeight;
+    renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+
+// lights
+light = new THREE.DirectionalLight( 0xffffff );
+light.position.set( 0, 1, 1 );
+scene.add( light );
+
+light = new THREE.AmbientLight( 0x222222 );
+scene.add( light );
+
+
+// EVENT LISTENER RESIZE
+window.addEventListener('resize',resize);
+resize();
+  
+// add player/player
+var playerSize = 10;
+var geometry = new THREE.SphereGeometry(playerSize, 32, 32);
+var material = new THREE.MeshPhongMaterial( {specular: "#ff5555", color: "#ff0000", emissive: "#ff0000", side: THREE.DoubleSide} );
+var player = new THREE.PlayerMesh(geometry, material);
+player.position.set(0,100,0);
+scene.add(player);
+
+// setting up the camera:
+var aspect = window.innerWidth/window.innerHeight;
+var camera = new THREE.PerspectiveCamera(45, aspect, 0.1, 20000);
+camera.position.set(100,250,350);
+camera.lookAt(scene.position); 
+player.add(camera);
+
+// setting controls
+var controls = new THREE.OrbitControls(camera);
+controls.enableDamping = true;
+controls.dampingFactor = 0.25;
+controls.enableZoom = false;
+
+// array to track allObstacles
+var allObstacles = [];
+
+// floor from p3 used
+var floorTexture = new THREE.ImageUtils.loadTexture( 'img/game/checkerboard.jpg' );
+floorTexture.wrapS = floorTexture.wrapT = THREE.RepeatWrapping; 
+floorTexture.repeat.set(10, 10);
+var floorMaterial = new THREE.MeshBasicMaterial( { map: floorTexture, side: THREE.DoubleSide } );
+var floorGeometry = new THREE.PlaneGeometry(400, 400, 10, 10);
+var floor = new THREE.Mesh(floorGeometry, floorMaterial);
+floor.position.y = -1 + playerSize;
+floor.rotation.x = Math.PI / 2;
+scene.add(floor);
+
+allObstacles.push(floor);
+
+//test obstacle
+var geometry = new THREE.BoxGeometry( 100, 40, 100 );
+var material = new THREE.MeshNormalMaterial();
+var cube = new THREE.PlatformMesh( geometry, material );
+cube.position.set(100,30,0);
+scene.add(cube);
+allObstacles.push(cube);
+
+//test obstacle
+var geometry = new THREE.BoxGeometry( 100, 40, 100 );
+var material = new THREE.MeshNormalMaterial();
+var cube = new THREE.PlatformMesh( geometry, material );
+cube.position.set(-100,30,0);
+scene.add(cube);
+allObstacles.push(cube);
+
+//test obstacle
+var geometry = new THREE.BoxGeometry( 100, 40, 100 );
+var material = new THREE.MeshNormalMaterial();
+var cube = new THREE.PlatformMesh( geometry, material );
+cube.position.set(0,150,0);
+scene.add(cube);
+allObstacles.push(cube);
+	
+// first person camera. 
+THREE.FirstPersonControls = function (){
+	controls.movementSpeed = 100;
+	controls.lookSpeed = 0.2;
+	controls.lookVertical = false;
+	controls.noFly = true;
+}
+
+// adding an object
+var keyboard = new THREEx.KeyboardState();
+var clock = new THREE.Clock(true);
+
+function keyboardCallBack() {
+	 var delta = clock.getDelta();
+	 var distanceMoved = 100 * delta;
+
+	 if(keyboard.pressed("W")  && player.collisions.z != -1){
+	 	 player.translateZ (-distanceMoved);
+	 }
+	 if(keyboard.pressed("A") && player.collisions.x != -1){
+	 	player.translateX(-distanceMoved);
+   }
+   if(keyboard.pressed("S")  && player.collisions.z != 1){
+      player.translateZ (distanceMoved);
+	 }
+   if(keyboard.pressed("D") && player.collisions.x != 1){
+   	player.translateX(distanceMoved);	 
+  }
+}
+
+function onKeyDown(event) {
+  if(keyboard.eventMatches(event,"space")){
+    player.jump();
+  }
+}
+keyboard.domElement.addEventListener('keydown', onKeyDown );
+
+function moveAllPlatforms() {
+  // the 0th obstacle is the floor, so start from 1
+  for (i = 1; i < allObstacles.length -1; i++) {
+    allObstacles[i].movePlatform();
+  }
+}
+
+//add new platform when player reaches the highest platform
+function addNewPlatform() {
+  if (player.position.y >= allObstacles[allObstacles.length-1].position.y)  {
+    var geometry = new THREE.BoxGeometry( 100, 40, 100 );
+    var material = new THREE.MeshNormalMaterial();
+    var cube = new THREE.PlatformMesh( geometry, material );
+
+    newPos = newPlatformPosition();
+    cube.position.set(newPos.x,newPos.y,newPos.z);
+    scene.add(cube);
+    allObstacles.push(cube);
+    newPlatformPosition();
+  }
+}
+
+// generates position of the new platform using the current highest platform
+function newPlatformPosition() {
+  //position of the highest platform
+  var oldPos = allObstacles[allObstacles.length-1].position;
+
+  var radius = Math.random()*75 + 75;
+  var angle1 = Math.random()*360;
+
+  var x = radius * Math.cos(angle1);
+  var z = radius * Math.sin(angle1);
+  var y = Math.random()* 50 + 75 + oldPos.y;
+  console.log(x,y,z);
+  return new THREE.Vector3(x,y,z);
+}
+
+var render = function() {
+ player.updatePosition();
+ keyboardCallBack();
+ moveAllPlatforms()
+ addNewPlatform();
+ cube.movePlatform();
+ requestAnimationFrame(render);
+ renderer.render(scene, camera);
+};
+
+render();
